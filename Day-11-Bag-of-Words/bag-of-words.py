53ae30d13d9841f43a0aa6d963737cdeeee763c8
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2024-04-29 13:47:05 +0700

    Add code

--- Day-11-Bag-of-Words/bag-of-words.py
+++ Day-11-Bag-of-Words/bag-of-words.py
@@ -1,31 +1,23 @@
 # String manipulation using list
 def main():
-    corpus = ["Tôi thích Toán",
-              "Tôi thích AI",
-              "Tôi thích âm nhạc"]
+    corpus = ["Tôi thích Toán", "Tôi thích AI", "Tôi thích âm nhạc"]
     # Create a vocabulary
-    vocabulary=[]
+    vocabulary = []
     for doc in corpus:
         for word in doc.split(" "):
             if word not in vocabulary:
                 vocabulary.append(word)
     print(vocabulary)
     # Bag-of-Words
-    given_str="Tôi thích AI thích Toán"
-    given_str_tokenized = given_str \
-                          .split(' ')
-    vector=[0 for _ in range(
-    	                len(vocabulary)
-    	                )]
+    given_str = "Tôi thích AI thích Toán"
+    given_str_tokenized = given_str.split(" ")
+    vector = [0 for _ in range(len(vocabulary))]
     for word in given_str_tokenized:
         if word in vocabulary:
-            idx=vocabulary.index(word)
-            vector[idx]+=1
+            idx = vocabulary.index(word)
+            vector[idx] += 1
     print(vector)
+
 
 if __name__ == "__main__":
     main()
-    
-    
-    	                  
-    
