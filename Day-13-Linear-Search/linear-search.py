b391edd3dc2ecbe7038ba41ed01ef93ffa9f9a41
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2024-04-29 13:46:27 +0700

    Add code

--- Day-13-Linear-Search/linear-search.py
+++ Day-13-Linear-Search/linear-search.py
@@ -1,22 +1,18 @@
 # Linear Search
 def linear_search(array, value):
-    idxs=[]
+    idxs = []
     for idx in range(len(array)):
-        if array[idx]==value:
+        if array[idx] == value:
             idxs.append(idx)
     return idxs
 
-def main():
-    lst_data=[1, 1.1, None, 1.4,
-              None, 1.5, None, 2.0]
-    none_idxs=linear_search(lst_data,
-    	                      None)
-    print(
-    	f"Vị trí None đầu tiên: {none_idxs[0]}")
-    print(
-    	f"Danh sách các vị trí có giá trị None: {none_idxs}")
 
-if __name__=="__main__":
+def main():
+    lst_data = [1, 1.1, None, 1.4, None, 1.5, None, 2.0]
+    none_idxs = linear_search(lst_data, None)
+    print(f"Vị trí None đầu tiên: {none_idxs[0]}")
+    print(f"Danh sách các vị trí có giá trị None: {none_idxs}")
+
+
+if __name__ == "__main__":
     main()
-    
-    
